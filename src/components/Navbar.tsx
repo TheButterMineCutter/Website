38275e17e38c65efd3b9b31516c57efd3f650c75
TheButterMineCutter <112914337+TheButterMineCutter@users.noreply.github.com>
2023-04-20 22:17:19 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-20 bg-transparent backdrop-blur-md border-b-2 border-white border-opacity-20 flex justify-center items-center fixed z-[30] top-0 hover:bg-white/5">
+    <div className="w-full h-20 bg-transparent backdrop-blur-md border-b-2 border-white border-opacity-20 flex justify-center items-center fixed z-[40] top-0 hover:bg-white/5">
       <Link className="text-white font-medium ml-12" to="/">Home</Link>
       <Link className="text-white font-medium ml-12" to="/projects/">Projects</Link>
     </div>
